e2f0d5a4d749fbdcc4bfcbe0b8e9c23856fc5474
Ricardo Ferreira <ricardoferreirades@gmail.com>
2022-07-08 00:21:12 +0200

    feat: add miragejs configuration
    Signed-off-by: Ricardo Ferreira <ricardoferreirades@gmail.com>

---
+++ src/services/api/mirage.ts
@@ -0,0 +1,36 @@
+import { createServer, Model, Response } from "miragejs";
+
+type environmentType = "development" | "testing" | "production";
+
+export function makeServer(environment: environmentType = "testing") {
+  const server = createServer<any, any>({
+    environment,
+
+    models: {
+      data: Model,
+    },
+
+    routes() {
+      this.namespace = "api";
+      this.get("/data", () => this.schema.all("data"));
+      this.post("/data", (schema, request) => {
+        const showError = Math.round(Math.random() * 10) > 5;
+        console.log(showError);
+
+        if (showError) {
+          return new Response(
+            500,
+            {},
+            { error: "Not possible to save the data" }
+          );
+        }
+
+        const { requestBody } = request;
+        const data = JSON.parse(requestBody);
+        return schema.create("data", data);
+      });
+    },
+  });
+
+  return server;
+}
