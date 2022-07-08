ec40bcf68e8ec0d213180965e4e98a9350752f38
Ricardo Ferreira <ricardoferreirades@gmail.com>
2022-07-08 20:21:39 +0200

    feat: add a table to load the items added
    Signed-off-by: Ricardo Ferreira <ricardoferreirades@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,57 @@ import { useEffect, useState } from "react";
 import { GenderForm } from "./components/GenderForm";
 import { IFormControls } from "./components/GenderForm/gender-form";
 import { Message } from "./components/Message";
-import { useHttpPost } from "./services/api/useHttpPost";
-import "./style.css";
+import { useHttpGet, useHttpPost } from "./services/api/useHttpPost";
+import { GlobalStyle } from "./styles/global";
 
 function App() {
   const { method, statusText, statusType } = useHttpPost("/api/data");
+  const { method: getMethod, response } = useHttpGet("/api/data");
   const [resetForm, setResetForm] = useState<boolean>(false);
   const handleSubmit = async (e: IFormControls) => {
-    await method();
+    await method<IFormControls>(e);
   };
 
   useEffect(() => {
     setResetForm(statusType === "success");
+    getMethod();
   }, [statusType]);
 
   return (
     <div className="App">
-      <GenderForm onSubmit={handleSubmit} reset={resetForm} />
+      <GlobalStyle />
+      <div className="form-area">
+        <GenderForm onSubmit={handleSubmit} reset={resetForm} />
+        {statusText && <Message message={statusText} type={statusType} />}
+      </div>
 
-      {statusText && <Message message={statusText} type={statusType} />}
+      <br />
+
+      <table>
+        <thead>
+          <tr>
+            <td>id</td>
+            <td>Name</td>
+            <td>Gender</td>
+            <td>Favorite Singer</td>
+            <td>Favorite Book</td>
+            <td>Favorite Sport</td>
+          </tr>
+        </thead>
+
+        <tbody>
+          {response?.data?.map((item: IFormControls) => (
+            <tr key={item.id}>
+              <td>{item.id}</td>
+              <td>{item.name}</td>
+              <td>{item.gender}</td>
+              <td>{item["favorite-singer"]}</td>
+              <td>{item["favorite-book"]}</td>
+              <td>{item["favorite-sport"]}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
--- src/components/GenderForm/gender-form.ts
+++ src/components/GenderForm/gender-form.ts
@@ -1,6 +1,7 @@
 export type GenderTypes = "male" | "female" | "non-binary";
 
 export interface IFormControls {
+  id?: number;
   name: string;
   gender: GenderTypes;
   "favorite-singer": string;
